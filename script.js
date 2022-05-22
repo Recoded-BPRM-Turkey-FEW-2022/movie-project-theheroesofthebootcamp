63ee369238c83dfde5f6017e8b27bf1944659e3a
Amjad <amjadmaqsuma@gmail.com>
2022-05-22 16:50:21 +0300

    Adding some comments to make the code easier to read

--- script.js
+++ script.js
@@ -6,12 +6,14 @@ const BACKDROP_BASE_URL = "http://image.tmdb.org/t/p/w780";
 const CONTAINER = document.querySelector(".container");
 
 // Don't touch this function please
+//FIRST: Everything starts here This represents the homePage
 const autorun = async () => {
   const movies = await fetchMovies();
   renderMovies(movies.results);
 };
 
 // Don't touch this function please
+//This one to handle the API key and integrate it with the path so we can access the API
 const constructUrl = (path) => {
   return `${TMDB_BASE_URL}/${path}?api_key=${atob(
     "NTQyMDAzOTE4NzY5ZGY1MDA4M2ExM2M0MTViYmM2MDI="
@@ -19,12 +21,15 @@ const constructUrl = (path) => {
 };
 
 // You may need to add to this function, definitely don't delete it.
+// THIRD:   As far as I understood...This function is used to fetch the ID of the movie to be used lately to fetch the details of the movie.
 const movieDetails = async (movie) => {
   const movieRes = await fetchMovie(movie.id);
   renderMovie(movieRes);
+  
 };
 
-// This function is to fetch movies. You may need to add it or change some part in it in order to apply some of the features.
+// This function is to fetch the list of the movies. You may need to add it or change some part in it in order to apply some of the features.
+//SECOND the Data is being fetched from the API
 const fetchMovies = async () => {
   const url = constructUrl(`movie/now_playing`);
   const res = await fetch(url);
@@ -32,6 +37,7 @@ const fetchMovies = async () => {
 };
 
 // Don't touch this function please. This function is to fetch one movie.
+//This is the function to fetch the RAW details of the movie after you click on the movie.
 const fetchMovie = async (movieId) => {
   const url = constructUrl(`movie/${movieId}`);
   const res = await fetch(url);
@@ -39,7 +45,10 @@ const fetchMovie = async (movieId) => {
 };
 
 // You'll need to play with this function in order to add features and enhance the style.
+//This shows the list of movies after being fectched from the API
+//It also connects the click event to the movieDetails function
 const renderMovies = (movies) => {
+  
   movies.map((movie) => {
     const movieDiv = document.createElement("div");
     movieDiv.innerHTML = `
@@ -51,11 +60,14 @@ const renderMovies = (movies) => {
       movieDetails(movie);
     });
     CONTAINER.appendChild(movieDiv);
+
   });
 };
 
 // You'll need to play with this function in order to add features and enhance the style.
+//This starts when you enter the movie page  to show the conent of the page
 const renderMovie = (movie) => {
+
   CONTAINER.innerHTML = `
     <div class="row">
         <div class="col-md-4">
